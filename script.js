038adb2e3795999f209bda2b522e84be2a94836c
Sadia Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2023-11-21 09:48:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-}
+const charCount = new Map();
+	for (let char of str) {
+		charCount.set(char,(charCount.get(char)||0)+1);
+	}
+	//find the first nonRepeating character in the orignal order
+	for (let char of str) {
+		if (charCount.get(char)===1){
+			return char;
+		}
+	}
+	return null;
+		
+	}
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
